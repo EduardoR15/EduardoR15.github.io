ed00e8a6683deedbf98c83d313b5b5a36e3a572c
Eduardo Marambio <eduardomarambio@MacBook-Air-de-Eduardo.local>
2022-03-29 13:22:23 -0300

    Ejercicio Promt

---
+++ script.js
@@ -0,0 +1,24 @@
+let numero = prompt("Ingrese numero");
+
+console.log("Tipo de dato: " + typeof numero)
+
+let pasarAnumero = parseInt(numero)
+
+let resultado = numero * 2;
+
+console.log("Tipo de dato: "+ typeof pasarAnumero)
+
+alert("El doble es: "+ resultado);
+
+
+let numero2 = prompt("Ingrese fecha nacimiento")
+
+let fechaNacimiento = parseInt(numero2);
+
+const anioActual = 2022;
+
+let edad = anioActual - numero2;
+
+console.log("Su edad es: " + edad);
+
+alert("Usted tiene " + edad +" años.");
